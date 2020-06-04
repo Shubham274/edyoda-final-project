02ef9c56b8154adffa06862d136706614254c338
Shubham Kaushik <skaushik274@gmail.com>
2020-06-04 17:28:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var bigWrapper = document.getElementsByClassName('info-big-wrapper')[0];
 var leftWrapper = document.getElementsByClassName('info-left-wrapper')[0];
 var closeBtn = document.getElementsByClassName('for-x')[0];
 
+var links = document.getElementsByTagName('a');
+
+for (var i = 0; i < links.length; i++) {
+	links[i].addEventListener('click', function(e) {
+		if (this.getAttribute('href') === '#') e.preventDefault();
+	});
+}
+
 for (var i = 0; i < freeQuote.length; i++) {
 	freeQuote[i].addEventListener('click', function(e) {
 		bigWrapper.style.display = 'block';
